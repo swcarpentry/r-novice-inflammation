cd79f26eb06d0e2fa4eb32c3b38675a18ce80016
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2021-04-15 00:52:02 +0100

    Actually revert match.arg, it's confusing and not general

--- episodes/readings-06.R
+++ episodes/readings-06.R
@@ -2,6 +2,7 @@ main <- function() {
   args <- commandArgs(trailingOnly = TRUE)
   action <- args[1]
   filenames <- args[-1]
+  stopifnot(action %in% c("--min", "--mean", "--max"))
   
   if (length(filenames) == 0) {
     process(file("stdin"), action)
@@ -14,7 +15,7 @@ main <- function() {
 
 process <- function(filename, action) {
   dat <- read.csv(file = filename, header = FALSE)
-  action <- match.arg(action, choices = c("--min", "--mean", "--max"))
+
   if (action == "--min") {
     values <- apply(dat, 1, min)
   } else if (action == "--mean") {
